d7f0ac836c774715f681ec91c9dcc7ea9914a648
Espen Kørra <ekorra@gmail.com>
2017-03-17 08:39:29 +0100

    fikset namespaces

---
+++ MoveAdmin.Web/Controllers/OrganisationsController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AltInnSrr;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoveAdmin.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class OrganisationsController : Controller
+    {
+        // GET api/organisations
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/organisations/5
+        [HttpGet("{id}")]
+        public async Task<string> Get(int id)
+        {
+            var org = new Organisation(this.HttpContext.RequestServices.GetService(typeof(ISrrClient)) as ISrrClient);
+            await org.GetInforation(id);
+            return org.ToJson();
+        }
+
+        // POST api/organisations
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/organisations/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/organisations/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- MoveAdmin.web/Startup.cs
+++ MoveAdmin.web/Startup.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceModel;
-using System.Threading.Tasks;
-using AltInnSrr;
+﻿using AltInnSrr;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace MoveAdmin.web
+namespace MoveAdmin.Web
 {
     public class Startup
     {
