bc2e689dab7f3b329c1f68d2786589256be27ce0
Espen Kørra <ekorra@gmail.com>
2017-06-19 13:25:36 +0200

    Added create

--- MoveAdmin.Web/Organisation.cs
+++ MoveAdmin.Web/Organisation.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using AltInnSrr;
 using Newtonsoft.Json;
 
@@ -18,19 +19,41 @@ namespace MoveAdmin.Web
         }
 
         public async Task GetInforation(int orgnr)
-        {
-           
+        {           
             AltInnSrrRights = await srrClient.GetRights(orgnr);
         }
-        
-        public string GetOrgName(string orgnr)
+       
+
+        public async Task Create(int orgnr)
         {
-            return null;
+            AltInnSrrRights = await srrClient.AddRights(orgnr);
+        }
+
+        public async Task Update(int orgnr, DateTime? validTo = null)
+        {
+            if (!validTo.HasValue)
+            {
+                validTo = DateTime.Now.AddYears(2);
+            }
+            AltInnSrrRights = await srrClient.UpdateRights(orgnr, validTo.Value);
+        }
+
+        public async Task Delete(int orgnr)
+        {
+            await srrClient.DeleteRights(orgnr);
         }
 
         public string ToJson()
         {
             return JsonConvert.SerializeObject(this);
+        }
+
+        public bool IsValidOrgnr(int orgnr)
+        {
+            if (orgnr.ToString().Length != 9)
+                return false;
+
+            return orgnr % 11 == 0;
         }
 
     }
